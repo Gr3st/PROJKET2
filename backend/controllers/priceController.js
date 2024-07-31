28bb04a55aeef934fbca9f9b32708af54766ccc7
Gr3st <118692156+Gr3st@users.noreply.github.com>
2024-07-31 21:46:32 +0000

    w

--- backend/controllers/priceController.js
+++ backend/controllers/priceController.js
@@ -13,8 +13,8 @@ exports.dodajCene = async (req, res) => {
     if (existingCeny) {
       return res.status(400).send('Cena already exists');
     }
-    const newCena1 = new Schemas.Ceny({ timeRange: "00:01", cena: "12" });
-    await newCena1.save();
+    // const newCena1 = new Schemas.Ceny({ timeRange: "00:01", cena: "12" });
+    // await newCena1.save();
     const newCena = new Schemas.Ceny({ timeRange: czas, cena });
     await newCena.save();
     res.send('Price added successfully');
