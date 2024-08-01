de6dd5656fa95b7cf7a07685ab3eb2cdb42ceeba
Gr3st <118692156+Gr3st@users.noreply.github.com>
2024-08-01 18:10:48 +0000

    test-v3

--- frontend/src/components/userTable.jsx
+++ frontend/src/components/userTable.jsx
@@ -84,8 +84,8 @@ function UserTable() {
           <div className="table-cell">{res.id}</div>
       
           {/* <div className="table-cell">{calculateOverdueTime(res.exitDate)>0?res.cena + calculateAdditionalCost(calculateOverdueTime(res.exitDate)):res.cena}</div> */}
-          <div className="table-cell">{res.countdown === calculateTimeDifference(res.entryDate, res.exitDate)?res.cena : res.cena + calculateAdditionalCost(calculateOverdueTime(res.exitDate))}</div>
-          
+          <div className="table-cell">{calculateTimeDifference(res.entryDate, res.exitDate)>=0?res.cena : res.cena + calculateAdditionalCost(calculateOverdueTime(res.exitDate))}</div>
+          {console.log(res.countdown+" "+calculateTimeDifference(res.entryDate, res.exitDate))}
           <div className="table-cell">
             {!res.exitDate ? (
               <div>
