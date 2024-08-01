5cfb5fc4c97f16b1853e6e5f696613b919d3856c
Gr3st <118692156+Gr3st@users.noreply.github.com>
2024-08-01 22:11:32 +0000

    as

--- frontend/src/components/userForm.jsx
+++ frontend/src/components/userForm.jsx
@@ -48,7 +48,7 @@ function UserForm() {
       <input type="text" placeholder="Nazwisko" value={nazwisko} onChange={(e) => setNazwisko(e.target.value)} onKeyDown={handleKeyPress} />
       <input type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} onKeyDown={handleKeyPress} />
       <input type="text" placeholder="ID" value={id} onChange={(e) => setId(e.target.value)} onKeyDown={handleKeyPress} />
-      <button onClick={(e)=>handleIdChange(e.target.value)}>STOP</button>
+      <button onClick={()=>handleIdChange(id)}>STOP</button>
 
       <div>
         Czas: 
