de7e254020d98fe21656aeb61074eac8a619cd2b
Gr3st <118692156+Gr3st@users.noreply.github.com>
2024-08-02 03:02:27 +0000

    asf

--- frontend/src/components/userTable.jsx
+++ frontend/src/components/userTable.jsx
@@ -98,7 +98,7 @@ function UserTable() {
             onChange={(e) => setSearch(e.target.value)}
             placeholder='search...'
           />
-          <button onClick={() => handleIdChange(uid)}>STOP</button>
+          <button onClick={() => handleIdChange(search)}>STOP</button>
         </div>
       </div>
       <div className="table-header">
